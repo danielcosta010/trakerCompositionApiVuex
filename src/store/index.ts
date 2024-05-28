44b0ef8ae8d64f714e7d25af4b679ffc22cd0838
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-28 15:01:49 -0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,8 +3,9 @@ import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import { InjectionKey } from 'vue'
 import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
-import { ALTERAR_PROJETO, CADASTRAR_PROJETO, OBTER_PROJETOS, REMOVER_PROJETO } from "./tipos-de-acoes";
-import http from '@/http'
+import { ALTERAR_PROJETO, CADASTRAR_PROJETO, OBTER_PROJETOS, REMOVER_PROJETO } from "./tipos-acoes";
+
+import http from "@/http"
 
 interface Estado {
     projetos: IProjeto[],
@@ -33,9 +34,8 @@ export const store = createStore<Estado>({
         [EXCLUIR_PROJETO](state, id: string) {
             state.projetos = state.projetos.filter(proj => proj.id != id)
         },
-        [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
+        [DEFINIR_PROJETOS] (state, projetos: IProjeto[]) {
             state.projetos = projetos
-            
         },
         [NOTIFICAR] (state, novaNotificacao: INotificacao) {
 
@@ -48,11 +48,11 @@ export const store = createStore<Estado>({
         }
     },
     actions: {
-        [OBTER_PROJETOS]({ commit }) {
+        [OBTER_PROJETOS] ({ commit }) {
             http.get('projetos')
-                .then(resposta => commit(DEFINIR_PROJETOS, resposta.data))
+                .then(response => commit(DEFINIR_PROJETOS, response.data))
         },
-        [CADASTRAR_PROJETO](contexto, nomeDoProjeto: string) {
+        [CADASTRAR_PROJETO] (contexto, nomeDoProjeto: string) {
             return http.post('/projetos', {
                 nome: nomeDoProjeto
             })
